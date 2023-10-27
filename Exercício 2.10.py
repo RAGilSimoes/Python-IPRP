a1ceb35025ecf6520d435adf26a16d87b585a562
Ricardo Simoes <ricardo.andre.simoes@gmail.com>
2023-10-27 10:14:40 +0100

    .

--- Exercício 2.10.py
+++ Exercício 2.10.py
@@ -4,22 +4,32 @@ from turtle import Screen
 Screen().bgcolor("lightgreen")
 turtle.color("red")
 
-angulo = 0
+escrever = ""
 
-def relogio(angulo):
-    for i in range(12):
-        turtle.setheading(angulo)
-        turtle.penup()
-        turtle.forward(250)
-        turtle.pendown()
-        turtle.forward(25)
-        turtle.penup()
-        turtle.setposition(0,0)
-        angulo += 30
+def relogio(angulo,escrever):
+    turtle.setheading(angulo)
+    turtle.penup()
+    turtle.forward(200)
+    turtle.pendown()
+    turtle.forward(25)
+    turtle.penup()
+    turtle.forward(50)
+    fonte2 = ("Comic Sans", 20, "normal") #para escrever coisas
+    turtle.write(escrever, False, "left", fonte2)
+    turtle.penup()
+    turtle.setposition(0,0)
         
-    #fonte2 = ("Comic Sans", 20, "normal") para escrever coisas
-    #turtle.write("de", False, "center", fonte2)
-        
-relogio(angulo)
+relogio(0,"3")
+relogio(30,"2")
+relogio(60,"1")
+relogio(90,"12")
+relogio(120,"11")
+relogio(150,"10")
+relogio(180,"9")
+relogio(210,"8")
+relogio(240,"7")
+relogio(270,"6")
+relogio(300,"5")
+relogio(330,"4")
 
 turtle.exitonclick()
