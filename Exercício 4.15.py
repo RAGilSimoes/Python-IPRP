2743f4bc5f16ba0d8b2d47a7ab050917c9be1af8
Ricardo Andre Simoes <ricardo.andre.simoes@gmail.com>
2023-10-29 12:59:50 +0000

    .

---
+++ Exercício 4.15.py
@@ -0,0 +1,16 @@
+palavra = input("Introduz um nome por extenso: ")
+tamanho = len(palavra)
+acronimo = ""
+i = 0
+
+acronimo = palavra[0]
+
+for letra in palavra:
+    if letra == " ":
+        
+        proxima = palavra[i+1]
+        acronimo = acronimo + proxima
+    i += 1
+    
+
+print(acronimo)
