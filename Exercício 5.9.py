62a89510d11866e068b775e127f4e5e6eaa23a73
Ricardo Andre Simoes <ricardo.andre.simoes@gmail.com>
2023-11-01 14:50:54 +0000

    .

---
+++ Exercício 5.9.py
@@ -0,0 +1,20 @@
+import random as r
+
+n = int(input("Introduz o número de vezes que o dado vai ser rodado: "))
+
+pares = 0
+impares = 0
+
+for i in range(n):
+    face = r.randint(1,6)
+    
+    if (face % 2 == 0):
+        pares += 1
+    else:
+        impares += 1
+        
+percentagem = pares / n
+
+print("saiu par: ", pares)
+print("saiu impar: ", impares)
+print("A percentagem de vezes em que saiu número par é: ", percentagem)
