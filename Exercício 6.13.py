8f65499b6171dd87a8e6361beacb1da3b170bcfb
Ricardo Andre Simoes <ricardo.andre.simoes@gmail.com>
2023-12-12 22:36:59 +0000

    .

---
+++ Exercício 6.13.py
@@ -0,0 +1,16 @@
+def dias_mes_ano(data):
+    dias_semana = {"1":"Domingo", "2":"Segunda-Feira", "3":"Terça-Feira", "4":"Quarta-Feira", "5":"Quinta-Feira", "6":"Sexta-Feira", "7":"Sábado"}
+    meses_ano = {"1":"Janeiro", "2":"Fevereiro", "3":"Março", "4":"Abril", "5":"Maio", "6":"Junho", "7":"Julho", "8":"Agosto", "9":"Setembro", "10":"Outubro", "11":"Novembro", "12":"Decembro"}
+    
+    datas = data.split("/")
+    
+    DS = dias_semana[datas[0]]
+    DM = datas[1]
+    M = meses_ano[datas[2]]
+    A = datas[3]
+    
+    data_final = DS + ", " + DM + " " + M + " " + A
+    
+    return data_final
+
+print(dias_mes_ano("4/5/6/2006"))
